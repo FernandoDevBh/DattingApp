a7e1a8eb994b4743ae9488389bb2944810a71491
FernandoDevBh <fsffernando2@gmail.com>
2021-12-11 08:15:07 -0300

    End of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,47 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using API.Entities;
+
+namespace API.Controllers
+{
+  public class BuggyController : BaseApiController
+  {
+    private readonly DataContext _context;
+
+    public BuggyController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [Authorize]
+    [HttpGet("Auth")]
+    public ActionResult<string> GetSecret()
+    {
+      return "secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+      var thing = _context.Users.Find(-1);
+      if (thing == null) return NotFound();
+
+      return Ok(thing);
+    }
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+      var thing = _context.Users.Find(-1);
+
+      var thingToReturn = thing.ToString();
+      return Ok(thingToReturn);
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+      return BadRequest("This was not a good request");
+    }    
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
